6ed9fd81be08b52fb45bea031924edcaf206631b
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-10 11:17:03 +0530

    Setup for tailwind css with ejs is completed

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+const indexRouter = require('./routes/index');
+
+const path = require('path');
+
+app.set('view engine', 'ejs');
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 app.use('/', indexRouter);
 
 
-app.listen(3000);
+app.listen(3000,()=>{
+  console.log('Server is running on port 3000');  // Server is running on port 3000
+});
