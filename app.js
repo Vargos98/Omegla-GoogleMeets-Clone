63f13e9ce8a0717ea1b1afdbbb106bafcf1551e3
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-10 11:50:15 +0530

    Updated index.ejs and initiated frontend to backend server connection

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const indexRouter = require('./routes/index');
-
 const path = require('path');
+
+const socketIO = require('socket.io');
+const http = require('http');
+const server = http.createServer(app)
+
+const io = socketIO(server)
+
+io.on("connection",(socket)=>{
+  
+})
 
 app.set('view engine', 'ejs');
 
@@ -16,6 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 
-app.listen(3000,()=>{
+server.listen(3000,()=>{
   console.log('Server is running on port 3000');  // Server is running on port 3000
 });
