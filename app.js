4951505f257a3865aabeeefd8c37a6c54f839706
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-10 12:58:01 +0530

    Implemented connection between 2 clients

--- app.js
+++ app.js
@@ -5,26 +5,36 @@ const path = require('path');
 
 const socketIO = require('socket.io');
 const http = require('http');
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-const io = socketIO(server)
+const io = socketIO(server);
 
-io.on("connection",(socket)=>{
-  
-})
+let waitingUsers = []; // Corrected variable name
+let rooms = {};
+
+io.on("connection", (socket) => {
+  socket.on("joinroom", () => {
+      if (waitingUsers.length > 0) {
+          let partner = waitingUsers.shift();
+          const roomname = `${socket.id}-${partner.id}`;
+          socket.join(roomname);
+          partner.join(roomname); // Fixed method to join room.
+
+          io.to(roomname).emit("joined");
+      } else {
+          waitingUsers.push(socket); // No changes needed here.
+      }
+  });
+});
 
 app.set('view engine', 'ejs');
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
 
 app.use('/', indexRouter);
 
-
-server.listen(3000,()=>{
-  console.log('Server is running on port 3000');  // Server is running on port 3000
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
